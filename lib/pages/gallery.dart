b3c6259da156f541b934e716b262442e07292336
alihashem8 <alihashem8@outlook.com>
2022-05-08 16:30:21 +0300

    cards and files

--- lib/pages/gallery.dart
+++ lib/pages/gallery.dart
@@ -19,9 +19,20 @@ class _GalleyWidgetState extends State<GalleyWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      //TODO: create list of folders and items as gallery, you can get items from services
-      child: Text(items.length.toString()),
+    return MaterialApp(
+      home: Scaffold(
+        body: GridView.builder(
+          itemCount: items.length,
+          // ignore: unrelated_type_equality_checks
+          itemBuilder: (context, index) => items[index].type == 'folder'
+              ? FolderTile(index)
+              : FileTile(index),
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 2,
+          ),
+        ),
+      ),
     );
   }
 
@@ -30,5 +41,91 @@ class _GalleyWidgetState extends State<GalleyWidget> {
     setState(() {
       items = items;
     });
+  }
+}
+
+class FolderTile extends StatelessWidget {
+  const FolderTile(int index);
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 40),
+          child: ListTile(
+            onTap: () {},
+            leading: const SizedBox(
+              width: 50,
+              height: 30,
+              child: Icon(
+                Icons.folder,
+                size: 68,
+              ),
+            ),
+          ),
+        ),
+        Row(
+          children: const [
+            Padding(
+              padding: EdgeInsets.fromLTRB(62, 24, 20, 0),
+              child: Text("folder"),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class FileTile extends StatelessWidget {
+  const FileTile(int index);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Padding(
+          padding: EdgeInsets.all(7),
+          child: Container(
+            width: 300,
+            height: 200,
+            padding: new EdgeInsets.all(10.0),
+            child: Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15.0),
+              ),
+              elevation: 10,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  ListTile(
+                    leading:
+                        Image.asset('https://picsum.photos/200/300/?random'),
+                    title: const Text(""),
+                  ),
+                  Row(
+                    children: const [
+                      Text("id"),
+                    ],
+                  ),
+                  Row(
+                    children: const [
+                      Text("name"),
+                    ],
+                  ),
+                  Row(
+                    children: const [
+                      Text("quantity"),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
