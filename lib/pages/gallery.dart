f5d192282db482fafb3a7c7d140423215c446e01
alihashem8 <alihashem8@outlook.com>
2022-05-10 14:03:44 +0300

    floating button

--- lib/pages/gallery.dart
+++ lib/pages/gallery.dart
@@ -1,8 +1,9 @@
-// ignore_for_file: type_init_formals, use_key_in_widget_constructors, must_be_immutable
+// ignore_for_file: type_init_formals, use_key_in_widget_constructors, must_be_immutable, prefer_const_constructors
 
 import 'package:flutter/material.dart';
 import 'package:younes_mobile/models/gallery-item.dart';
 import 'package:younes_mobile/services/gallery-items.dart';
+import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 
 class GalleyWidget extends StatefulWidget {
   const GalleyWidget({Key? key}) : super(key: key);
@@ -23,6 +24,112 @@ class _GalleyWidgetState extends State<GalleyWidget> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        floatingActionButton: SizedBox(
+          height: 40,
+          width: 40,
+          child: FloatingActionButton(
+            backgroundColor: Colors.blue[500],
+            onPressed: () => showMaterialModalBottomSheet(
+              context: context,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(40),
+                    topRight: Radius.circular(40)),
+              ),
+              builder: (context) => SingleChildScrollView(
+                controller: ModalScrollController.of(context),
+                child: Container(
+                    margin: EdgeInsets.fromLTRB(10, 10, 10, 20),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [],
+                          ),
+                        ),
+                        Container(
+                            padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: const [
+                                Text("Items:",
+                                    style: TextStyle(
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.bold)),
+                              ],
+                            )),
+                        Container(
+                          padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                          child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: const [
+                                Text("Total Amount:",
+                                    style: TextStyle(
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.bold)),
+                                SizedBox(height: 10),
+                                TextField(
+                                    decoration: InputDecoration(
+                                  hintText: "0",
+                                  border: OutlineInputBorder(),
+                                )),
+                              ]),
+                        ),
+                        Container(
+                          padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                          child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: const [
+                                Text("Additional Notes:",
+                                    style: TextStyle(
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.bold)),
+                                SizedBox(height: 10),
+                                TextField(
+                                    maxLines: 3,
+                                    decoration: InputDecoration(
+                                      border: OutlineInputBorder(),
+                                    )),
+                              ]),
+                        ),
+                        Container(
+                            margin: EdgeInsets.fromLTRB(0, 10, 0, 0),
+                            child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  FloatingActionButton.extended(
+                                    onPressed: () {
+                                      Navigator.of(context).pop();
+                                    },
+                                    backgroundColor: Colors.blue[400],
+                                    label: Text(
+                                      "Cancel",
+                                      style: TextStyle(fontSize: 14),
+                                    ),
+                                  ),
+                                  SizedBox(width: 10),
+                                  FloatingActionButton.extended(
+                                    onPressed: null,
+                                    backgroundColor: Colors.blue[400],
+                                    label: Text(
+                                      "Add",
+                                      style: TextStyle(fontSize: 14),
+                                    ),
+                                  ),
+                                ])),
+                      ],
+                    )),
+              ),
+            ),
+            child: const Icon(
+              Icons.add,
+              size: 30,
+            ),
+          ),
+        ),
         body: GridView.builder(
           itemCount: items.length,
           itemBuilder: (context, index) => items[index].type == 'folder'
@@ -52,7 +159,6 @@ class FolderTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: [
         Padding(
           padding: const EdgeInsets.only(left: 40),
@@ -96,41 +202,38 @@ class FileTile extends StatelessWidget {
             borderRadius: BorderRadius.circular(10.0),
           ),
           elevation: 10,
-          child: SizedBox(
-            height: 400,
-            child: Column(
-              children: <Widget>[
-                Row(children: [
-                  const Padding(padding: EdgeInsets.all(8)),
-                  Image.network(
-                    item.image.toString(),
-                    fit: BoxFit.fill,
-                    height: 25,
-                    width: 25,
-                  ),
-                ]),
-                Row(children: [
-                  const Padding(padding: EdgeInsets.all(8)),
-                  Text(
-                    item.name,
-                    style: const TextStyle(fontSize: 13),
-                  )
-                ]),
-                Row(children: [
-                  const Padding(padding: EdgeInsets.all(8)),
-                  Text(
-                    "Quantity: " + item.quantity.toString(),
-                    style: const TextStyle(fontSize: 11),
-                  )
-                ]),
-                //    onTap: () {
-                //      Navigator.push(context, MaterialPageRoute(builder: (_) {
-                //        return DetailScreen(item);
-                //       }));
-                //     },
-                //   ),
-              ],
-            ),
+          child: Column(
+            children: <Widget>[
+              Row(children: [
+                const Padding(padding: EdgeInsets.all(8)),
+                Image.network(
+                  item.image.toString(),
+                  fit: BoxFit.fill,
+                  height: 25,
+                  width: 25,
+                ),
+              ]),
+              Row(children: [
+                const Padding(padding: EdgeInsets.all(8)),
+                Text(
+                  item.name,
+                  style: const TextStyle(fontSize: 13),
+                )
+              ]),
+              Row(children: [
+                const Padding(padding: EdgeInsets.all(8)),
+                Text(
+                  "Quantity: " + item.quantity.toString(),
+                  style: const TextStyle(fontSize: 11),
+                )
+              ]),
+              //    onTap: () {
+              //      Navigator.push(context, MaterialPageRoute(builder: (_) {
+              //        return DetailScreen(item);
+              //       }));
+              //     },
+              //   ),
+            ],
           )),
     );
   }
