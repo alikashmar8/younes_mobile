03963e8ddf1d585c897671ee250c9520c0155bdd
alihashem8 <alihashem8@outlook.com>
2022-05-11 22:34:50 +0300

    image

--- lib/common/base-api.service.dart
+++ lib/common/base-api.service.dart
@@ -2,9 +2,9 @@
 
 abstract class BaseApiService {
   final String baseUrl =
-      'http://ip172-18-0-118-c9tpr114lkkg008ovmp0-3000.direct.labs.play-with-docker.com/';
+      'http://ip172-18-0-58-c9tu8mg9jotg00bising-3000.direct.labs.play-with-docker.com/api/';
   final String apiUrl =
-      'http://ip172-18-0-118-c9tpr114lkkg008ovmp0-3000.direct.labs.play-with-docker.com/api/';
+      'http://ip172-18-0-58-c9tu8mg9jotg00bising-3000.direct.labs.play-with-docker.com/api/';
 
   Future<dynamic> getResponse(String url, String access_token);
   Future<dynamic> postResponse(String url, Map<String, String> jsonBody);
--- lib/widgets/dialogs.dart
+++ lib/widgets/dialogs.dart
@@ -1,5 +1,7 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:spinner_input/spinner_input.dart';
+import 'package:image_picker/image_picker.dart';
 
 
 enum ViewDialogsAction { yes, no }
@@ -7,6 +9,7 @@ enum ViewDialogsAction { yes, no }
 class ViewDialogs {
   static Future<void> addItemDialog(BuildContext context) async {
     double spinner = 0;
+    File _image;
 
     final action = await showDialog(
       context: context,
@@ -18,10 +21,16 @@ class ViewDialogs {
           ),
           title: const Text('Add Item'),
           content: Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
-            const TextField(
-              decoration: InputDecoration(
-                labelText: 'Item Name',
-              ),
+            Row(
+              children: const [
+                Text("Add an Image"),
+                SizedBox(width: 10),
+                ExpandIcon(
+                  // Icons.add_a_photo,
+                  size: 30,
+                  onPressed: cameraImage,
+                ),
+              ],
             ),
             const TextField(
               decoration: InputDecoration(
@@ -75,96 +84,12 @@ class ViewDialogs {
     );
     return action;
   }
-
-  // SingleChildScrollView(
-  //   // controller: ModalScrollController.of(context),
-  //   child: Scaffold(
-  //     body: Container(
-  //         margin: EdgeInsets.fromLTRB(10, 10, 10, 20),
-  //         child: Column(
-  //           crossAxisAlignment: CrossAxisAlignment.start,
-  //           children: [
-  //             Container(
-  //               padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
-  //               child: Column(
-  //                   crossAxisAlignment: CrossAxisAlignment.start,
-  //                   children: const [
-  //                     Text("Name:",
-  //                         style: TextStyle(
-  //                             fontSize: 15, fontWeight: FontWeight.bold)),
-  //                     SizedBox(height: 10),
-  //                     TextField(
-  //                         decoration: InputDecoration(
-  //                       border: OutlineInputBorder(),
-  //                     )),
-  //                   ]),
-  //             ),
-  //             Container(
-  //               padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
-  //               child: Column(
-  //                   crossAxisAlignment: CrossAxisAlignment.start,
-  //                   children: [
-  //                     Text("Quantity:",
-  //                         style: TextStyle(
-  //                             fontSize: 15, fontWeight: FontWeight.bold)),
-  //                     SizedBox(height: 10),
-  //                     SpinnerInput(
-  //                       spinnerValue: spinner,
-  //                       minValue: 0,
-  //                       maxValue: 200,
-  //                       onChange: (newValue) {
-  //                         // setState(() {
-  //                         //   spinner = newValue;
-  //                         // });
-  //                       },
-  //                     ),
-  //                   ]),
-  //             ),
-  //             Container(
-  //               padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
-  //               child: Column(
-  //                   crossAxisAlignment: CrossAxisAlignment.start,
-  //                   children: const [
-  //                     Text("Description:",
-  //                         style: TextStyle(
-  //                             fontSize: 15, fontWeight: FontWeight.bold)),
-  //                     SizedBox(height: 10),
-  //                     TextField(
-  //                         maxLines: 3,
-  //                         decoration: InputDecoration(
-  //                           border: OutlineInputBorder(),
-  //                         )),
-  //                   ]),
-  //             ),
-  //             Container(
-  //                 margin: EdgeInsets.fromLTRB(0, 10, 0, 0),
-  //                 child: Row(
-  //                     mainAxisAlignment: MainAxisAlignment.center,
-  //                     children: [
-  //                       FloatingActionButton.extended(
-  //                         onPressed: () {
-  //                           Navigator.of(context).pop();
-  //                         },
-  //                         backgroundColor: Colors.blue[400],
-  //                         label: Text(
-  //                           "Cancel",
-  //                           style: TextStyle(fontSize: 14),
-  //                         ),
-  //                       ),
-  //                       SizedBox(width: 10),
-  //                       FloatingActionButton.extended(
-  //                         onPressed: null,
-  //                         backgroundColor: Colors.blue[400],
-  //                         label: Text(
-  //                           "Add",
-  //                           style: TextStyle(fontSize: 14),
-  //                         ),
-  //                       ),
-  //                     ])),
-  //           ],
-  //         )),
-  //   ),
-  // ),
-  //   );
-  // }
+  static Future<void> cameraImage(bool value) async {
+    print("aaa");
+    var image = await ImagePicker.pickImage(
+      source: ImageSource.gallery,
+      maxHeight: 240.0,
+      maxWidth: 240.0,
+    );
+  }
 }
