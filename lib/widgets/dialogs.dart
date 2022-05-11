a2355542ba70145de5e9ff1b66bfdaaf1192c51a
alihashem8 <alihashem8@outlook.com>
2022-05-11 23:28:14 +0300

    add image

--- lib/widgets/dialogs.dart
+++ lib/widgets/dialogs.dart
@@ -25,9 +25,10 @@ class ViewDialogs {
               children: const [
                 Text("Add an Image"),
                 SizedBox(width: 10),
-                ExpandIcon(
-                  // Icons.add_a_photo,
-                  size: 30,
+                IconButton(
+                  icon: Icon(Icons.add_a_photo),
+                  iconSize: 30,
+                  color: Colors.blue,
                   onPressed: cameraImage,
                 ),
               ],
@@ -51,6 +52,7 @@ class ViewDialogs {
                   ),
                   const SizedBox(width: 10),
                   SpinnerInput(
+                    middleNumberPadding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
                     spinnerValue: spinner,
                     minValue: 0,
                     maxValue: 200,
@@ -104,103 +106,11 @@ class ViewDialogs {
     );
   }
 
-  static Future<void> cameraImage(bool value) async {
-    print("aaa");
+  static Future<void> cameraImage() async {
     var image = await ImagePicker.pickImage(
       source: ImageSource.gallery,
       maxHeight: 240.0,
       maxWidth: 240.0,
     );
   }
-  // SingleChildScrollView(
-  //   // controller: ModalScrollController.of(context),
-  //   child: Scaffold(
-  //     body: Container(
-  //         margin: EdgeInsets.fromLTRB(10, 10, 10, 20),
-  //         child: Column(
-  //           crossAxisAlignment: CrossAxisAlignment.start,
-  //           children: [
-  //             Container(
-  //               padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
-  //               child: Column(
-  //                   crossAxisAlignment: CrossAxisAlignment.start,
-  //                   children: const [
-  //                     Text("Name:",
-  //                         style: TextStyle(
-  //                             fontSize: 15, fontWeight: FontWeight.bold)),
-  //                     SizedBox(height: 10),
-  //                     TextField(
-  //                         decoration: InputDecoration(
-  //                       border: OutlineInputBorder(),
-  //                     )),
-  //                   ]),
-  //             ),
-  //             Container(
-  //               padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
-  //               child: Column(
-  //                   crossAxisAlignment: CrossAxisAlignment.start,
-  //                   children: [
-  //                     Text("Quantity:",
-  //                         style: TextStyle(
-  //                             fontSize: 15, fontWeight: FontWeight.bold)),
-  //                     SizedBox(height: 10),
-  //                     SpinnerInput(
-  //                       spinnerValue: spinner,
-  //                       minValue: 0,
-  //                       maxValue: 200,
-  //                       onChange: (newValue) {
-  //                         // setState(() {
-  //                         //   spinner = newValue;
-  //                         // });
-  //                       },
-  //                     ),
-  //                   ]),
-  //             ),
-  //             Container(
-  //               padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
-  //               child: Column(
-  //                   crossAxisAlignment: CrossAxisAlignment.start,
-  //                   children: const [
-  //                     Text("Description:",
-  //                         style: TextStyle(
-  //                             fontSize: 15, fontWeight: FontWeight.bold)),
-  //                     SizedBox(height: 10),
-  //                     TextField(
-  //                         maxLines: 3,
-  //                         decoration: InputDecoration(
-  //                           border: OutlineInputBorder(),
-  //                         )),
-  //                   ]),
-  //             ),
-  //             Container(
-  //                 margin: EdgeInsets.fromLTRB(0, 10, 0, 0),
-  //                 child: Row(
-  //                     mainAxisAlignment: MainAxisAlignment.center,
-  //                     children: [
-  //                       FloatingActionButton.extended(
-  //                         onPressed: () {
-  //                           Navigator.of(context).pop();
-  //                         },
-  //                         backgroundColor: Colors.blue[400],
-  //                         label: Text(
-  //                           "Cancel",
-  //                           style: TextStyle(fontSize: 14),
-  //                         ),
-  //                       ),
-  //                       SizedBox(width: 10),
-  //                       FloatingActionButton.extended(
-  //                         onPressed: null,
-  //                         backgroundColor: Colors.blue[400],
-  //                         label: Text(
-  //                           "Add",
-  //                           style: TextStyle(fontSize: 14),
-  //                         ),
-  //                       ),
-  //                     ])),
-  //           ],
-  //         )),
-  //   ),
-  // ),
-  //   );
-  // }
 }
