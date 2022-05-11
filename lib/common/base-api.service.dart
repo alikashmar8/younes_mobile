86746afee75a8859faca9b3272f18bd1f91bccbe
alihashem8 <alihashem8@outlook.com>
2022-05-11 18:57:26 +0300

    image and dialog trial

--- lib/common/base-api.service.dart
+++ lib/common/base-api.service.dart
@@ -2,9 +2,9 @@
 
 abstract class BaseApiService {
   final String baseUrl =
-      'http://ip172-18-0-36-c9tm9f14lkkg008ovdgg-3000.direct.labs.play-with-docker.com/';
+      'http://ip172-18-0-118-c9tpr114lkkg008ovmp0-3000.direct.labs.play-with-docker.com/';
   final String apiUrl =
-      'http://ip172-18-0-36-c9tm9f14lkkg008ovdgg-3000.direct.labs.play-with-docker.com/api/';
+      'http://ip172-18-0-118-c9tpr114lkkg008ovmp0-3000.direct.labs.play-with-docker.com/api/';
 
   Future<dynamic> getResponse(String url, String access_token);
   Future<dynamic> postResponse(String url, Map<String, String> jsonBody);
--- lib/pages/gallery.dart
+++ lib/pages/gallery.dart
@@ -4,13 +4,12 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/get_instance/src/extension_instance.dart';
 import 'package:get/utils.dart';
-import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 import 'package:younes_mobile/controllers/gallery-items.controller.dart';
 import 'package:younes_mobile/models/gallery-item.dart';
 import 'package:younes_mobile/widgets/dialogs.dart';
 import 'package:younes_mobile/widgets/file-card.widget.dart';
 import 'package:younes_mobile/widgets/folder-card.widget.dart';
-import 'package:spinner_input/spinner_input.dart';
+import 'dart:io';
 
 class GalleyWidget extends StatefulWidget {
   const GalleyWidget({Key? key}) : super(key: key);
@@ -22,7 +21,6 @@ class GalleyWidget extends StatefulWidget {
 class _GalleyWidgetState extends State<GalleyWidget> {
   // List<GalleryItem> items = [];
   final GalleryItemsController _controller = Get.put(GalleryItemsController());
-  double spinner = 0;
 
   bool tappedYes = false;
 
@@ -32,8 +30,76 @@ class _GalleyWidgetState extends State<GalleyWidget> {
     // initializeData();
   }
 
+  _GalleyWidgetState({this.onTap, this.imagePath});
+  final Function()? onTap;
+  final String? imagePath;
+
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    if (imagePath != null) {
+      return Card(
+        child: Container(
+          height: 300,
+          padding: EdgeInsets.all(10.0),
+          width: size.width * .70,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(4.0)),
+            image: DecorationImage(
+                fit: BoxFit.cover, image: FileImage(File(imagePath as String))),
+          ),
+          
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.redAccent,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black,
+                      offset: Offset(3.0, 3.0),
+                      blurRadius: 2.0, 
+                    )
+                  ]
+                ),
+                child: IconButton(onPressed: (){
+                print('icon press');
+              }, icon: Icon(Icons.delete, color: Colors.white)),
+              )
+            ],
+          ),
+        ),
+      );
+    }
+
+    return Card(
+        elevation: 3,
+        child: InkWell(
+          onTap: this.onTap,
+          child: Container(
+            padding: EdgeInsets.symmetric(vertical: 18, horizontal: 25),
+            width: size.width * .70,
+            height: 100,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Attach Picture',
+                  style: TextStyle(fontSize: 17.0, color: Colors.grey[600]),
+                ),
+                Icon(
+                  Icons.photo_camera,
+                  color: Colors.indigo[400],
+                )
+              ],
+            ),
+          ),
+        ));
     return Scaffold(
         floatingActionButton: SizedBox(
           height: 40,
